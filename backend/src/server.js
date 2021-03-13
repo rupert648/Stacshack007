dfd950a8ed877122cbbcbd2ef3c8cf4687e12148
rupert648 <47698091+rupert648@users.noreply.github.com>
2021-03-13 14:40:12 +0000

    added in map to get source information of news items

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const axios = require('axios').default;
 
 
 const logger = require('./util/logger');
@@ -50,7 +51,35 @@ app.get('/api/news', (req, res, next) => {
         q: ['environment', 'global warming'],
         language: 'en'
     }).then(response => {
-        res.json(response)
+        let numbResults = response.totalResults
+        
+        let newResponse = response.articles.map(article => {
+            // include source information in response
+            let sourceInfo = {}
+            if (article.source.id) {
+                for (let i = 0; i < sources.length; i++) {
+                    if (sources[i].id === article.source.id) {
+                        sourceInfo = sources[i];
+                        break;
+                    }
+                }
+            } else {
+                for (let i = 0; i < sources.length; i++) {
+                    if (sources[i].name === article.source.name) {
+                        sourceInfo = sources[i];
+                        break;
+                    }
+                }
+            }
+            
+            return {
+                ...article,
+                sourceInfo
+            }
+        })
+
+        res.json(newResponse)
+
     })
     // res.json({response:'hi'})
 });
@@ -66,8 +95,20 @@ app.use('*', (req, res) => {
     .json( {status: false, message: 'Endpoint Not Found'} );
 })
 
+// get info of sources
+
+let sources = {}
+
+newsapi.v2.sources({
+    language: 'en'
+}).then(response => {
+    sources = response.sources
+});
+
 // Open Server on selected Port
 app.listen(
     PORT,
-    () => console.info('Server listening on port ', PORT)
+    () => {
+        console.info('Server listening on port ', PORT)
+    }
 );
