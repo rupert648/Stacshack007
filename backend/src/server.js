acb28e45004107a2e88d230df0f30cd41b61dfba
rupert648 <47698091+rupert648@users.noreply.github.com>
2021-03-13 18:38:29 +0000

    implemented feed

--- backend/src/server.js
+++ backend/src/server.js
@@ -67,7 +67,6 @@ app.get('/api/news', (req, res, next) => {
         page: pageNumb
     }).then(response => {
         let numbResults = response.totalResults
-        pageNumb++
         let newResponse = response.articles.map(article => {
             // include source information in response
             let sourceInfo = {}
